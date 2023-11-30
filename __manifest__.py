8d31d505c0621e8316d1feaf937e4f532d4c9c69
nandovs2 <nyarla.hoteps@gmail.com>
2023-11-30 20:05:55 +0700

    create models

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Odoo Custom Module',
+    'version': '1.0',
+    'category': 'Custom',
+    'author': 'nando',
+    'summary': 'Custom module',
+    'description': "This is custom module",
+    'depends': [],
+    'data': [],
+    'installable': True,
+
+}
