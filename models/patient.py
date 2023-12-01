5b20aba0f708fd938a6e3702ff93a9b9931be980
nandovs2 <nyarla.hoteps@gmail.com>
2023-12-01 08:42:17 +0700

    add comments

--- models/patient.py
+++ models/patient.py
@@ -2,9 +2,10 @@ from odoo import fields, models, api
 
 
 class HospitalPatient(models.Model):
-    _name = 'hospital.patient'
-    _description = 'Hospital Patient'
+    _name = 'hospital.patient' #nama dari model/tabel
+    _description = 'Hospital Patient' #deskripsi model/tabel
 
+    #field dari model/tabel (column)
     name = fields.Char(string='Name')
     age = fields.Integer(string='Age')
     gender = fields.Selection([('male','Male'),('female','Female')],string='Gender')
